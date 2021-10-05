edac6a1b4de4118cb8ff3b0bd64ce0d28303c7d3
Alec <movieornasasim@gmail.com>
2021-10-05 12:23:27 -0500

    Adjusted some color settings and added a header for the header sections

--- Assets/_Editor/DialogueToolWindow.cs
+++ Assets/_Editor/DialogueToolWindow.cs
@@ -15,8 +15,8 @@ public class DialogueToolWindow : EditorWindow
 
     Color _headerSectionColor = new Color(13f/255f, 32f/255f, 44f/255f, 1f);
     Color _textHeaderColor = new Color(56f/255f, 12f/255f, 68f/255f, 1f);
-    Color _nameHeaderColor = new Color(14f/255f, 65f/255f, 30f/255f, 1f);
-    Color _portraitHeaderColor = new Color(14f/255f, 65f/255f, 30f/255f, 1f);
+    Color _nameHeaderColor = new Color(45f/255f, 65f/255f, 30f/255f, 1f);
+    Color _portraitHeaderColor = new Color(90f/255f, 2f/255f, 90f/255f, 1f);
 
 
     [MenuItem("Window/Dialogue Tool")]
@@ -37,24 +37,27 @@ public class DialogueToolWindow : EditorWindow
     private void OnGUI()
     {
         DrawLayouts();
+        DrawHeader();
     }
 
     void InitTextures()
     {
+
+        //CHANGE TO ASSETDATABASE AS A SOURCE FOR TEXTURES
         _headerSectionTexture = new Texture2D(1, 1);
         _headerSectionTexture.SetPixel(0,0, _headerSectionColor);
         _headerSectionTexture.Apply();
 
-        _textHeaderTexture= new Texture2D(1, 1);
-        _textHeaderTexture.SetPixel(0, 0, _textHeaderColor);
+        _textHeaderTexture = new Texture2D(1, 1);
+        _textHeaderTexture.SetPixel(0, 1, _textHeaderColor);
         _textHeaderTexture.Apply();
 
         _nameHeaderTexture = new Texture2D(1, 1);
-        _nameHeaderTexture.SetPixel(0, 0, _nameHeaderColor);
+        _nameHeaderTexture.SetPixel(1, 0, _nameHeaderColor);
         _nameHeaderTexture.Apply();
 
         _portraitHeaderTexture = new Texture2D(1,1);
-        _portraitHeaderTexture.SetPixel(0, 0, _portraitHeaderColor);
+        _portraitHeaderTexture.SetPixel(2, 3, _portraitHeaderColor);
         _portraitHeaderTexture.Apply();
     }
 
@@ -70,12 +73,12 @@ public class DialogueToolWindow : EditorWindow
         _textHeader.width = Screen.width / 3;
         _textHeader.height = Screen.height - 50;
 
-        _nameHeader.x = (Screen.width / 3);
+        _nameHeader.x = Screen.width / 3;
         _nameHeader.y = 50;
-        _nameHeader.width = (Screen.width / 3);
+        _nameHeader.width = Screen.width / 3;
         _nameHeader.height = Screen.height - 50;
 
-        _portraitHeader.x = (Screen.width / 3) * 2;
+        _portraitHeader.x = Screen.width / 3 * 2;
         _portraitHeader.y = 50;
         _portraitHeader.width = Screen.width / 3;
         _portraitHeader.height = Screen.height - 50;
@@ -84,5 +87,14 @@ public class DialogueToolWindow : EditorWindow
         GUI.DrawTexture(_textHeader, _textHeaderTexture);
         GUI.DrawTexture(_nameHeader, _nameHeaderTexture);
         GUI.DrawTexture(_portraitHeader, _portraitHeaderTexture);
+    }
+
+    void DrawHeader()
+    {
+        GUILayout.BeginArea(_sectionHeader);
+
+        GUILayout.Label("Dialogue Tool");
+
+        GUILayout.EndArea();
     }
 }
