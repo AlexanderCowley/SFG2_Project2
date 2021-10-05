7a0fc1d2c835e1a55478d92c7f59569a7edd28ee
Alec <movieornasasim@gmail.com>
2021-10-05 01:25:22 -0500

    Added information to the DialogueData ScriptableObject
    Added an array of strings for the collection of dialogue, a string variable for the name of the character speaking, a sprite for a portrait image and a float for text speed.

--- Assets/_Editor/DialogueData.cs
+++ Assets/_Editor/DialogueData.cs
@@ -1,6 +1,11 @@
+using UnityEngine.UI;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Dialogue Data", menuName = "New Dialogue Data")]
 public class DialogueData : ScriptableObject
 {
-    
+    public string[] dialogues;
+    public string characterName;
+    public Sprite portrait;
+    public float textSpeed;
 }
